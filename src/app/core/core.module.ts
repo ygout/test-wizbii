99292cda68d1ce672117f36fe7f6bfddb8c4119a
Yohann Goutaret <yohann.goutaret@gmail.com>
2020-11-15 18:47:53 +0100

    add condition core module constructor singleton

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,12 +1,19 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Optional, SkipSelf } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    HttpClientModule
+  ],
+  exports: [HttpClientModule]
 })
-export class CoreModule { }
+export class CoreModule {
+  construcor(@Optional() @SkipSelf() parentModule: CoreModule){
+    if(parentModule) {
+      throw new Error("Core module is already loaded ...");
+    }
+  }
+}
